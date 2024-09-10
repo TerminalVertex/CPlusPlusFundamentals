6391dea11d03ed8cbc4d2dd9238c469fcc20bb14
TerminalVertex <rickuss@gmail.com>
2024-09-10 13:41:47 +0900

    4.25 Enemy Run Animation
    -Migrated some code to base character class
    -Eliminated unnecessary state (screenPos), a derived value.
    -Function getScreenPos now calculates this value from worldPos in base class as pure virtual
    -BaseCharacter is now an abstract class.

--- BaseCharacter.h
+++ BaseCharacter.h
@@ -12,15 +12,13 @@ public:
 	Vector2 getWorldPos() { return worldPos; }
 	void undoMovement();
 	Rectangle getCollisionRec();
-
 	virtual void tick(float deltaTime);
-
+	virtual Vector2 getScreenPos() = 0; //abstract class, no longer make instance of base, only derive child classes
 protected:
 
 	Texture2D texture{ LoadTexture("characters/knight_idle_spritesheet.png") };
 	Texture2D idle{ LoadTexture("characters/knight_idle_spritesheet.png") };
 	Texture2D run{ LoadTexture("characters/knight_run_spritesheet.png") };
-	Vector2 screenPos{};
 	Vector2 worldPos{};
 	Vector2 worldPosLastFrame{};
 	// 1 : facing right, -1 : facing left
@@ -34,6 +32,7 @@ protected:
 	float width{};
 	float height{};
 	float scale{ 4.0f };
+	Vector2 velocity{};
 
 
 private:
